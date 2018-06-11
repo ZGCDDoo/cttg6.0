b17f4f22b402ebfecd2ad111279fbed3d1c29da0
charles-david hebert <charles-david.hebert@usherbrooke.ca>
2018-06-10 21:57:18 -0400

    Corrected esthetics of PrintVersion.hpp

--- src/Includes/PrintVersion.hpp
+++ src/Includes/PrintVersion.hpp
@@ -10,16 +10,16 @@ std::string gitBranch  = GIT_BRANCH;
 std::string gitHash = GIT_COMMIT_HASH;
 
 
-std::cout << "\t\t ============= CTTG5.2 ============= " << std::endl;
-std::cout << "\t\t\t -----------------------------------------" << std::endl; 
-std::cout << "\t\t\t -----------------------------------------" << std::endl; 
+std::cout << "\t\t\t\t ============= CTTG5.2 =============" << std::endl;
+std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
+std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
 std::cout << "\t\t\t\t\t\t .... \n \n " ;
 std::cout << "\t\t\t\t\t Git Branch = " << gitBranch << std::endl;
 std::cout << "\t\t\t\t\t gitHash = " << gitHash << std::endl;
 std::cout << "\t\t\t\t\t\t .... \n \n" ;
-std::cout << "\t\t\t -----------------------------------------" << std::endl; 
-std::cout << "\t\t\t -----------------------------------------" << std::endl; 
-std::cout << "\t\t =================================== " << std::endl;
+std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
+std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
+std::cout << "\t\t\t\t ===================================" << std::endl;
 std::cout << "\n\n\n";
 
 }
