bec8f9496ac7f51b9a3143916f36400364924ed4
charles-david hebert <charles-david.hebert@usherbrooke.ca>
2018-06-10 22:40:51 -0400

    Esthetics PrintVersion.hpp

--- src/Includes/PrintVersion.hpp
+++ src/Includes/PrintVersion.hpp
@@ -6,22 +6,21 @@ namespace PrintVersion
 void PrintVersion()
 {
 
-std::string gitBranch  = GIT_BRANCH;
-std::string gitHash = GIT_COMMIT_HASH;
+    std::string gitBranch = GIT_BRANCH;
+    std::string gitHash = GIT_COMMIT_HASH;
 
-
-std::cout << "\t\t\t\t ============= CTTG5.2 =============" << std::endl;
-std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
-std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
-std::cout << "\t\t\t\t\t\t .... \n \n " ;
-std::cout << "\t\t\t\t\t Git Branch = " << gitBranch << std::endl;
-std::cout << "\t\t\t\t\t gitHash = " << gitHash << std::endl;
-std::cout << "\t\t\t\t\t\t .... \n \n" ;
-std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
-std::cout << "\t\t\t\t -----------------------------------" << std::endl; 
-std::cout << "\t\t\t\t ===================================" << std::endl;
-std::cout << "\n\n\n";
-
+    std::cout << "\n\n\n";
+    std::cout << "\t\t\t\t ============= CTTG5.2 =============" << std::endl;
+    std::cout << "\t\t\t\t -----------------------------------" << std::endl;
+    std::cout << "\t\t\t\t -----------------------------------" << std::endl;
+    std::cout << "\t\t\t\t\t\t .... \n \n ";
+    std::cout << "\t\t\t\t\t Git Branch = " << gitBranch << std::endl;
+    std::cout << "\t\t\t\t\t gitHash = " << gitHash << std::endl;
+    std::cout << "\t\t\t\t\t\t .... \n \n";
+    std::cout << "\t\t\t\t -----------------------------------" << std::endl;
+    std::cout << "\t\t\t\t -----------------------------------" << std::endl;
+    std::cout << "\t\t\t\t ===================================" << std::endl;
+    std::cout << "\n\n\n";
 }
 
-};
+}; // namespace PrintVersion
