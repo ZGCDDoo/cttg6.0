d990c94df3ae95409fff585ef2f4b1f724d930ce
Charles-David Hebert <charles-david.hebert@usherbrooke.ca>
2018-06-11 20:43:47 -0400

    Replaced GreenTau, for paralle version that works even if NWorkers() < NindepSites. Needs more testing, not 100% sure yet.

--- src/Includes/Utilities/GreenTau.hpp
+++ src/Includes/Utilities/GreenTau.hpp
@@ -95,19 +95,36 @@ class GreenCluster0Tau
     void BuildParallel()
     {
         mpi::communicator world;
-        size_t indepSiteIndex = mpiUt::Rank();
-        Vector_t g0Tau;
 
-        if (indepSiteIndex < ioModel_.indepSites().size())
+        std::vector<Data_t> dataVec;
+        size_t ii = 0;
+        while (ii * mpiUt::NWorkers() < ioModel_.indepSites().size() + mpiUt::NWorkers())
         {
-            g0Tau = BuildOneGTau(indepSiteIndex);
+            size_t indepSiteIndex = mpiUt::Rank() + ii * mpiUt::NWorkers();
+            Vector_t g0Tau;
+
+            if (indepSiteIndex < ioModel_.indepSites().size())
+            {
+                g0Tau = BuildOneGTau(indepSiteIndex);
+            }
+
+            Data_t dataResult;
+            mpi::all_gather(world, g0Tau, dataResult);
+            dataVec.push_back(dataResult);
+            ii++;
         }
 
-        Data_t dataResult;
-        mpi::all_gather(world, g0Tau, dataResult);
-        for (size_t ii = 0; ii < ioModel_.indepSites().size(); ii++)
+        //There will be empty vectors, important not to count them here.
+        size_t jj = 0;
+        for (size_t ll = 0; ll < dataVec.size() && jj < ioModel_.indepSites().size(); ll++)
         {
-            data_.at(ii) = dataResult.at(ii);
+
+            for (size_t kk = 0; kk < dataVec.at(ll).size() && jj < ioModel_.indepSites().size(); kk++)
+            {
+                const size_t index = kk + jj;
+                data_.at(index) = dataVec.at(ll).at(kk);
+                jj++;
+            }
         }
     }
 #endif
