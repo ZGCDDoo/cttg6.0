e963119d5614611f0a676b80088a17c55b387897
Charles-David Hebert <hebertcharlesdavid@outlook.com>
2018-11-24 21:50:44 -0500

    Trying some tests in travis.

---
+++ travis_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir build && cmake .. && make \
+&& ./IntegratorTests            \
+&& ./MarkovChainTests           \
+&& ./MatrixTests                \
+&& ./SelfConsistencyTests       \
+&& ./ObservablesTests           \ 
+&& ./FourierTests               \
+&& ./Fourier_DCATests
+
+
+cd .. && mkdir build_mpi && cd build_mpi && cmake -DMPI_BUILD=ON .. && make
